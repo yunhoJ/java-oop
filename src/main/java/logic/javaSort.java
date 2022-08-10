cf0e01f92a0cbc421300e7c3c5580f7520385da6
yunho <wjse0213@naver.com>
2022-08-11 00:27:49 +0900

    Implement javasort
    자바 정렬 구현 및 테스트

---
+++ src/main/java/logic/javaSort.java
@@ -0,0 +1,14 @@
+package logic;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class javaSort <T extends Comparable<T>> {
+
+    public List<T> sort(List<T> list){
+        List<T> output =new ArrayList<T>(list);
+        Collections.sort(output);
+        return output;
+    }
+}
